740976e5dc318512b5811fd201c7a1b55d1cf5b0
Casey Slaught <caseyslaught@gmail.com>
2021-05-12 14:42:52 +0200

    updated GlobalSat function based on manufacturer documentation

--- GlobalSat.js
+++ GlobalSat.js
@@ -1,92 +1,74 @@
 function Decode(fPort, bytes, variables) {
-  function dec2bin(dec) {
-    return (dec >>> 0).toString(2);
+  var gpsFixStatus = Math.floor(bytes[1] / 64);
+  var reportType = bytes[1] % 64;
+  var batteryCapacity = bytes[2];
+
+  var d = (bytes[3] << 24) | (bytes[4] << 16) | (bytes[5] << 8) | bytes[6];
+  var e = (bytes[7] << 24) | (bytes[8] << 16) | (bytes[9] << 8) | bytes[10];
+
+  var latitude = parseInt(d) * 0.000001;
+  var longitude = parseInt(e) * 0.000001;
+
+  var decoded = {};
+  decoded.batteryCapacity = batteryCapacity;
+  decoded.longitude = longitude;
+  decoded.latitude = latitude;
+
+  switch (gpsFixStatus) {
+    case 0:
+      decoded.gpsFixStatus = "not fix";
+      break;
+    case 1:
+      decoded.gpsFixStatus = "2D";
+      break;
+    case 2:
+      decoded.gpsFixStatus = "3D";
+      break;
+    default:
+      decoded.gpsFixStatus = "n/a";
   }
 
-  function bitsToInt(bits) {
-    var result = 0;
-    bits.forEach(function (bite, index) {
-      result += bite * Math.pow(2, Math.abs(index - bits.length + 1));
-    });
-    return result;
+  switch (reportType) {
+    case 2:
+      decoded.reportType = "Period mode report";
+      break;
+    case 4:
+      decoded.reportType = "Motion mode static report";
+      break;
+    case 5:
+      decoded.reportType = "Motion mode moving report";
+      break;
+    case 6:
+      decoded.reportType = "Motion mode static to moving report";
+      break;
+    case 7:
+      decoded.reportType = "Motion mode moving to static report";
+      break;
+    case 14:
+      decoded.reportType = "Help report";
+      break;
+    case 15:
+      decoded.reportType = "Low battery alarm report";
+      break;
+    case 17:
+      decoded.reportType = "Power on (temperature)";
+      break;
+    case 19:
+      decoded.reportType = "Power off (low battery)";
+      break;
+    case 20:
+      decoded.reportType = "Power off (temperature)";
+      break;
+    case 24:
+      decoded.reportType = "Fall advisory report";
+      break;
+    case 27:
+      decoded.reportType = "Fpending report";
+      break;
+    default:
+      decoded.reportType = "n/a";
   }
 
-  var payloadByte = "";
-  bytes.forEach(function (byteInt) {
-    payloadByte += dec2bin(byteInt);
-  });
-
-  var parsedPayload = {};
-
-  var GPSFixStatusCode = bitsToInt(payloadByte.substring(8, 11).split(""));
-  if (GPSFixStatusCode === 0) {
-    parsedPayload.GPSFixStatus = "not fix";
-  } else if (GPSFixStatusCode == 1) {
-    parsedPayload.GPSFixStatus = "2D";
-  } else if (GPSFixStatusCode == 3) {
-    parsedPayload.GPSFixStatus = "3D";
-  } else {
-    parsedPayload.GPSFixStatus = "N/A";
-  }
-
-  var reportTypeCode = bitsToInt(payloadByte.substring(11, 16).split(""));
-  if (reportTypeCode == 2) {
-    parsedPayload.reportType = "Periodic mode report";
-  } else if (reportTypeCode == 4) {
-    parsedPayload.reportType = "Motion mode static report";
-  } else if (reportTypeCode == 5) {
-    parsedPayload.reportType = "Motion mode moving report";
-  } else if (reportTypeCode == 6) {
-    parsedPayload.reportType = "Motion mode static to motion report";
-  } else if (reportTypeCode == 7) {
-    parsedPayload.reportType = "Motion mode moving to static report";
-  } else if (reportTypeCode == 14) {
-    parsedPayload.reportType = "SOS alarm report";
-  } else if (reportTypeCode == 15) {
-    parsedPayload.reportType = "Low battery alarm report";
-  } else if (reportTypeCode == 17) {
-    parsedPayload.reportType = "Power on(temperature)";
-  } else if (reportTypeCode == 19) {
-    parsedPayload.reportType = "Power off(low battery)";
-  } else if (reportTypeCode == 20) {
-    parsedPayload.reportType = "Power off(temperature)";
-  } else {
-    parsedPayload.reportType = "N/A";
-  }
-
-  parsedPayload.battery =
-    bitsToInt(payloadByte.substring(16, 24).split("")) + "%";
-
-  var invertedLat = "";
-  if (payloadByte.charAt(24) == "1") {
-    payloadByte
-      .substring(24, 56)
-      .split("")
-      .forEach(function (bite, index) {
-        //if negative temperature, need to use the two's complement of the payload
-        invertedLat += Math.abs(1 - bite);
-      });
-    parsedPayload.lat = -bitsToInt(invertedLat.split("")) * 0.000001;
-  } else {
-    parsedPayload.lat =
-      bitsToInt(payloadByte.substring(24, 56).split("")) * 0.000001;
-  }
-
-  var invertedLng = "";
-  if (payloadByte.charAt(56) == "1") {
-    payloadByte
-      .substring(56, 88)
-      .split("")
-      .forEach(function (bite, index) {
-        //if negative temperature, need to use the two's complement of the payload
-        invertedLng += Math.abs(1 - bite);
-      });
-    parsedPayload.lng = -bitsToInt(invertedLng.split("")) * 0.000001;
-  } else {
-    parsedPayload.lng =
-      bitsToInt(payloadByte.substring(56, 88).split("")) * 0.000001;
-  }
-
-  print("parsedPayload: " + JSON.stringify(parsedPayload, null, "\t"));
-  return parsedPayload;
+  console.log("decoded payload: " + JSON.stringify(decoded, null, "\t"));
+  return decoded;
 }
